194da0a9224947d642c0e1041c30408ab90ebdb2
BuriKizilkaya <burak.kizilkaya@outlook.com>
2023-03-04 08:53:07 +0000

    Update app to a library

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -4,6 +4,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(TOOLCHAIN_PREFIX arm-none-eabi-)
 set(CMAKE_C_FLAGS_INIT "-fdata-sections -ffunction-sections --specs=nano.specs -Wl,--gc-sections")
 set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
@@ -15,8 +16,6 @@ set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
 set(CMAKE_EXECUTABLE_SUFFIX_ASM ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_CXX ".elf")
-
-set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(ENABLE_PLATFORM_BUILD ON)
 set(ENABLE_TESTS OFF)
